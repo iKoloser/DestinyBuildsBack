43d7f12244a47b3b65bb79ad449f372c1f817cfc
iKoloser <jordirodriguezherrera@gmail.com>
2024-07-29 23:39:36 +0200

    Register created and working

---
+++ DestinyBuildsBack/Migrations/20240729212916_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DestinyBuildsBack.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "667c4f1f-51ff-4398-9748-7998b2dda846", null, "User", "USER" },
+                    { "6c15ed7a-14f7-4d72-8f5a-46ad78c7b5b7", null, "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "667c4f1f-51ff-4398-9748-7998b2dda846");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "6c15ed7a-14f7-4d72-8f5a-46ad78c7b5b7");
+        }
+    }
+}
